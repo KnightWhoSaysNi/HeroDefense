6326b08063d9cf01f4f974a7313ec70b01265daa
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-09-10 14:45:05 +0200

    Created LevelEditor and EditorHelper. LevelElement and WaveElement created for Level and Wave respectively

---
+++ Assets/Scripts/Helpers/LevelElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Holds level information for a specific wave.
+/// </summary>
+[System.Serializable]
+public class LevelElement
+{
+    public Wave wave;
+    /// <summary>
+    /// How many times the wave will play in a level. Default should be 1 as anything less makes no sense.
+    /// </summary>
+    public int waveCount = 1;
+}
--- Assets/Scripts/Helpers/WaveElement.cs
+++ Assets/Scripts/Helpers/WaveElement.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Holds wave information for an enemy type.
+/// Holds wave information for a specific enemy.
 /// </summary>
 [System.Serializable]
-public class WaveEnemyData
+public class WaveELement
 {
     /// <summary>
-    /// Enemy that could appear in this wave.
+    /// Enemy that could appear in a wave.
     /// </summary>
-    public EnemyData enemyData;
+    public Enemy enemy;
+
     /// <summary>
-    /// Minimum number of enemies that could appear in this wave.
+    /// Minimum number of enemies that could appear in a wave.
     /// </summary>
     [Range(1, 1000)] public int minNumberOfEnemies; // ADD TO CONST
     /// <summary>
-    /// Maximum number of enemies that could appear in this wave.
+    /// Maximum number of enemies that could appear in a wave.
     /// </summary>
     [Range(1, 1000)] public int maxNumberOfEnemies; // ADD TO CONST
     /// <summary>
-    /// Chance of enemies (even) appearing in this wave, regardless of their number. Default is 1 (100%).
+    /// Chance of enemies (even) appearing in a wave, regardless of their number. Default is 1 (100%).
     /// </summary>
     [Range(0, 1)] public float chanceOfAppearing = 1; // ADD TO CONST
     public float delayBeforeSpawning = 0;
